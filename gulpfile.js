ed2e73653674a50b8a9dafb790116d47bd59ee73
minami takao <minami@waveltd.co.jp>
2014-09-11 12:44:04 +0900

    gulp installでサンプルが立ち上がるよう調整中

--- gulpfile.js
+++ gulpfile.js
@@ -46,34 +46,15 @@ gulp.task('server',function(){
   Sass compail
 */
 gulp.task('sassy',function(){
-  return gulp.src(ASSET_SASS+'/**/*.scss')
+  return gulp.src([ASSET_SASS+'/**/*.scss','!'+ASSET_SASS+'/bootstrap.scss'])
       .pipe($.rubySass(SASS_ARG))
       .on('error', function (err) { console.log(err.message); })
       .pipe($.autoprefixer(AUTOPREFIXER_BROWSERS))
       .pipe(gulp.dest(DOCUMENT_ROOT));
 });
-/*
-  Bootstrap bower_compornents to some folsers
-*/
-gulp.task('copystrap', function () {
-  //fonts copy
-  gulp.src('bower_components/bootstrap-sass-official/assets/fonts/**/*')
-    .pipe(gulp.dest(DOCUMENT_ROOT+'/fonts'))
-  //images copy
-  gulp.src('bower_components/bootstrap-sass-official/assets/images/**/*')
-    .pipe(gulp.dest(DOCUMENT_ROOT+'/img/bootstrap'))
-  //scss copy
-  gulp.src('bower_components/bootstrap-sass-official/assets/stylesheets/**/*')
-    .pipe(gulp.dest(ASSET_SASS+'/vendor/bootstrap'))
-  //javascript copy
-  gulp.src('bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.js')
-    .pipe($.uglify({ preserveComments: 'all' }))
-    .pipe($.rename('bootstrap.min.js'))
-    .pipe(gulp.dest(DOCUMENT_ROOT+'/js/bootstrap'))
-  gulp.src('bower_components/jquery/dist/jquery.min.js')
-    .pipe($.rename('jquery.latest.min.js'))
-    .pipe(gulp.dest(DOCUMENT_ROOT+'/js'))
-});
+
+gulp.task('install', ['bower_default','install_strap']);
+
 /* ----------------------------------
   tasks
 ----------------------------------- */
@@ -95,6 +76,77 @@ gulp.task('imagemin', function () {
 //       })
 //     .pipe(gulp.dest(DOCUMENT_ROOT+'/fonts/'));
 // });
+
+/*
+  Bootstrap bower_compornents to some folsers
+*/
+gulp.task('install_strap', ['copy_sass'], function () {
+  //sass compile
+  gulp.src(ASSET_SASS+'/bootstrap.scss')
+      .pipe($.rubySass(SASS_ARG))
+      .on('error', function (err) { console.log(err.message); })
+      .pipe($.autoprefixer(AUTOPREFIXER_BROWSERS))
+      .pipe($.minifyCss({keepBreaks:true}))
+      .pipe($.rename('bootstrap.min.css'))
+      .pipe(gulp.dest(DOCUMENT_ROOT+'/css'));
+  //fonts copy
+  gulp.src('bower_components/bootstrap-sass-official/assets/fonts/**/*')
+    .pipe(gulp.dest(DOCUMENT_ROOT+'/fonts'))
+  //images copy
+  gulp.src('bower_components/bootstrap-sass-official/assets/images/**/*')
+    .pipe(gulp.dest(DOCUMENT_ROOT+'/img/bootstrap'))
+  //javascript copy
+  gulp.src('bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.js')
+    .pipe($.uglify({ preserveComments: 'all' }))
+    .pipe($.rename('bootstrap.min.js'))
+    .pipe(gulp.dest(DOCUMENT_ROOT+'/js/bootstrap'))
+});
+gulp.task('copy_sass', function () {
+  //scss copy
+  return gulp.src('bower_components/bootstrap-sass-official/assets/stylesheets/**/*')
+    .pipe(gulp.dest(ASSET_SASS+'/vendor/bootstrap'))
+});
+
+gulp.task('bower_default', function () {
+  //jquery1.* - for IE8
+  gulp.src('bower_components/jquery-legacy/dist/jquery.min.js')
+    .pipe($.rename('jquery.legacy.min.js'))
+    .pipe(gulp.dest(DOCUMENT_ROOT+'/js'))
+  //jquery2.*
+  gulp.src('bower_components/jquery-modern/dist/jquery.min.js')
+    .pipe($.rename('jquery.min.js'))
+    .pipe(gulp.dest(DOCUMENT_ROOT+'/js'))
+  //modernizr
+  gulp.src('bower_components/modernizr/modernizr.js')
+      .pipe($.uglify({ preserveComments: 'all' }))
+      .pipe($.rename('modernizr.min.js'))
+      .pipe(gulp.dest(DOCUMENT_ROOT+'/js/lib'));
+  //selectivizr
+  gulp.src('bower_components/selectivizr/selectivizr.js')
+      .pipe($.uglify({ preserveComments: 'all' }))
+      .pipe($.rename('selectivizr.min.js'))
+      .pipe(gulp.dest(DOCUMENT_ROOT+'/js/lib'));
+  //respond 
+  gulp.src('bower_components/respond/dest/respond.min.js')
+      .pipe(gulp.dest(DOCUMENT_ROOT+'/js/lib'));
+  //jquery-cookie
+  gulp.src('bower_components/jquery.cookie/jquery.cookie.js')
+      .pipe($.uglify({ preserveComments: 'all' }))
+      .pipe($.rename('jquery.cookie.min.js'))
+      .pipe(gulp.dest(DOCUMENT_ROOT+'/js/lib'));
+  //magnific-popup
+  gulp.src('bower_components/magnific-popup/dist/*')
+      .pipe(gulp.dest(DOCUMENT_ROOT+'/js/lib/magnific-popup'));
+  //matchHeight 
+  gulp.src('bower_components/matchHeight/jquery.matchHeight-min.js')
+      .pipe(gulp.dest(DOCUMENT_ROOT+'/js/lib'));
+  //bxslider
+  gulp.src('bower_components/bxslider-4/jquery.bxslider.min.js')
+      .pipe(gulp.dest(DOCUMENT_ROOT+'/js/lib/bxslider'));
+  gulp.src('bower_components/bxslider-4/jquery.bxslider.css')
+      .pipe(gulp.dest(DOCUMENT_ROOT+'/js/lib/bxslider'));
+});
+
 /* ----------------------------------
   methods
 ----------------------------------- */
